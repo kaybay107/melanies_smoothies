2b68fa8d23c26b1a6c6c19e2137c0f3f9d07f17e
kaybay107 <kayla.ballew@nghs.com>
2025-06-19 10:04:10 -0400

    Update streamlit_app.py
    updating API once more

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,8 +43,8 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    st.write(ingredients_list)
-    st.text(ingredients_list)
+    #st.write(ingredients_list)
+    #st.text(ingredients_list)
 
     ingredients_string = ''
 
@@ -54,7 +54,7 @@ if ingredients_list:
         #st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/" +search_on)
+        smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/all" +search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
