04e06822e6f51cabf6626bbde4793217c4a235d1
kaybay107 <kayla.ballew@nghs.com>
2025-06-19 10:06:29 -0400

    Update streamlit_app.py
    indention error

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ if ingredients_list:
                 smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/all" +search_on)
                 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-    st.write(ingredients_string)
+        st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
